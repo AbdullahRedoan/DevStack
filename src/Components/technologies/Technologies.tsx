b39b235e627d216db53dbdb3b52c60a23e0e4f9e
Abdullah Redoan <abdullahredoanarb@gmail.com>
2026-09-11 02:54:37 +0600

    banner done

--- src/Components/technologies/Technologies.tsx
+++ src/Components/technologies/Technologies.tsx
@@ -1,6 +1,7 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { ITechnology } from "../../types/technologyType";
 import TechnologyCard from "./TechnologyCard";
+import YourStack from "./YourStack";
 
 interface technologiesProps {
   technologiesPromise: Promise<ITechnology[]>;
@@ -8,14 +9,36 @@ interface technologiesProps {
 
 const Technologies = ({ technologiesPromise }: technologiesProps) => {
   const technologies = use(technologiesPromise);
+  const [selectedStack, setSelectedStack] = useState();
   return (
-    <div className="flex">
-        
-      <div className=" grid grid-cols-3 gap-5 m-10">
-        {technologies.map((technology: ITechnology) => {
-          return <TechnologyCard technology={technology}></TechnologyCard>;
-        })}
-        <h2>Explore the Technologies</h2>
+    <div className=" w-9/10 mx-auto">
+      <div className="mb-8 text-left">
+        <h2 className="text-3xl sm:text-4xl font-extrabold tracking-tight text-slate-900">
+          Explore the{" "}
+          <span className="text-purple-500">Technologies</span>
+        </h2>
+        <p className="mt-2 text-base text-slate-500 dark:text-slate-400">
+          Pick one technology per category to build your ideal stack.
+        </p>
+      </div>
+      <div className="grid grid-cols-4">
+        <div className=" grid grid-cols-3 col-span-3 gap-5 my-10">
+          {technologies.map((technology: ITechnology) => {
+            return (
+              <TechnologyCard
+                selectedStack={selectedStack}
+                setSelectedStack={setSelectedStack}
+                technology={technology}
+              ></TechnologyCard>
+            );
+          })}
+        </div>
+        <div>
+          <YourStack
+            selectedStack={selectedStack}
+            technology={technologies}
+          ></YourStack>
+        </div>
       </div>
     </div>
   );
--- src/Components/technologies/YourStack.tsx
+++ src/Components/technologies/YourStack.tsx
@@ -1,10 +1,31 @@
-export interface YourStackProps {
-    technology: string
-}
+import type { Dispatch, SetStateAction } from "react";
+import type { ITechnology } from "../../types/technologyType";
 
-export default function YourStack({ technology }: YourStackProps) {
-    
+export interface YourStackProps {
+    technology: ITechnology[]
+    selectedStack: ITechnology[];
+    setSelectedStack: Dispatch<SetStateAction<ITechnology[]>>
+}
+export default function YourStack({ technology, selectedStack }: YourStackProps) {
+    const isEmpty:ITechnology[] = []
+    console.log(technology);
     return(
-        <div>Your Stack here</div>
+        <div className="bg-white border rounded-2xl p-6">
+      <h2 className="text-xl font-bold m-0">Your Stack</h2>
+      <small className="m-0">{isEmpty ? "No technologies selected yet." : `${selectedStack.length} Technology Selected`}</small>
+
+      {isEmpty ? (
+        /* Render Empty State Box */
+        <div className="border border-dashed rounded-xl my-4 py-6 text-center text-gray-400">
+            <small> Your stack is empty.</small>
+        </div>
+      ) : (
+        /* Render Stack Items List & Remove All Button */
+        <div>
+          {/* List items */}
+        </div>
+      )}
+    </div>
+        
     )
 }
